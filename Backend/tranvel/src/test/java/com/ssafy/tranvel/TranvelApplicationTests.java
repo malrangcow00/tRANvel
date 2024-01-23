c37ff21e587c610ca7bbae5ac774229974c60c32
JP <malrangcow00@gmail.com>
2024-01-23 17:03:30 +0900

    feat: "set environment"
    Change-Id: I48863b068e771d4823d6b258a3abdb8915b79160

---
+++ Backend/tranvel/src/test/java/com/ssafy/tranvel/TranvelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.tranvel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranvelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
