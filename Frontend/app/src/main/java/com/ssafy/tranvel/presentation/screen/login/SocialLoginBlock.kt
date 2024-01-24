3327ee98d92a41352f325a5e24b68d5628b63df2
안성준 <anseongjun@anseongjun-ui-macbookpro.local>
2024-01-25 02:36:30 +0900

    feat: 로그인 화면 개발
    구글 로그인 이미지 추가, 버튼 색 지정
    
    task: S10P12D203-36
    Change-Id: I7b3dfef88b08ac5a8ca2a3a2dea39bb25f12b011

--- Frontend/app/src/main/java/com/ssafy/tranvel/presentation/screen/login/SocialLoginBlock.kt
+++ Frontend/app/src/main/java/com/ssafy/tranvel/presentation/screen/login/SocialLoginBlock.kt
@@ -1,26 +1,29 @@
 package com.ssafy.tranvel.presentation.screen.login
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.ssafy.tranvel.R
 import com.ssafy.tranvel.presentation.screen.login.component.ImageButtonComponent
 
 @Preview
 @Composable
 fun SocialLoginBlock() {
-    Column(
-    ) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ){
         ImageButtonComponent(
-            id = R.drawable.kakao_login_medium_wide,
+            id = R.drawable.kakao_login_large,
             info = "카카오로그인"
         )
+
         ImageButtonComponent(
-            id = R.drawable.kakao_login_medium_wide,
-            info = "카카오로그인"
+            id = R.drawable.android_light_sq_ctn,
+            info = "구글 로그인"
         )
     }
 }
--- Frontend/app/src/main/java/com/ssafy/tranvel/presentation/screen/login/component/TextButtonComponent.kt
+++ Frontend/app/src/main/java/com/ssafy/tranvel/presentation/screen/login/component/TextButtonComponent.kt
@@ -1,24 +1,32 @@
 package com.ssafy.tranvel.presentation.screen.login.component
 
 import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.material.ripple.LocalRippleTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.ssafy.tranvel.presentation.ui.theme.RippleCustomTheme
 import com.ssafy.tranvel.presentation.ui.theme.TextColor
 
 @Composable
 fun TextButtonComponent(
     info: String
 ) {
-    TextButton(
-        onClick = {
-            Log.d("MYTAG", "눌렸음1")
-        },
-
-    ) {
-        Text(
-            text = info,
-            color = TextColor
-        )
+    CompositionLocalProvider(LocalRippleTheme provides RippleCustomTheme) {
+        TextButton(
+            onClick = {
+                Log.d("MYTAG", "눌렸음1")
+            },
+            modifier = Modifier.background(color = Color.White)
+        ) {
+            Text(
+                text = info,
+                color = TextColor
+            )
+        }
     }
 }
