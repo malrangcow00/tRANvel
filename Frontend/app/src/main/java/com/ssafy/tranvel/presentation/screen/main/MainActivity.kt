006148bf8e20de1523f71f753bde6022b2673933
안성준 <hiahn777@gmail.com>
2024-01-24 12:44:40 +0900

    패키지 기본 설정 완료
    Change-Id: Ic90b15b9c47b86969b9b9ab05953f677ef163831

--- Frontend/app/src/main/java/com/ssafy/tranvel/presentation/screen/main/MainActivity.kt
+++ Frontend/app/src/main/java/com/ssafy/tranvel/presentation/screen/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ssafy.tranvel
+package com.ssafy.tranvel.presentation.screen.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.ssafy.tranvel.ui.theme.TRANvelTheme
+import com.ssafy.tranvel.presentation.ui.theme.TRANvelTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- Frontend/app/src/main/java/com/ssafy/tranvel/presentation/ui/theme/Color.kt
+++ Frontend/app/src/main/java/com/ssafy/tranvel/presentation/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.ssafy.tranvel.ui.theme
+package com.ssafy.tranvel.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
