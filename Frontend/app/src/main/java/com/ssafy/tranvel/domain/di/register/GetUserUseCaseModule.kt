05c61730ffeddede35913621a64c23441114c0ec
안성준 <hiahn777@gmail.com>
2024-01-29 11:11:17 +0900

    feat: 로그인 로직 개발
    로그인 로직 개발 중
    
    task: S10P12D203-41
    Change-Id: Id7e60f925b86f27247c7ae79ad657bed8f8ded63

--- Frontend/app/src/main/java/com/ssafy/tranvel/domain/di/register/GetUserUseCaseModule.kt
+++ Frontend/app/src/main/java/com/ssafy/tranvel/domain/di/register/GetUserUseCaseModule.kt
@@ -2,13 +2,14 @@ package com.ssafy.tranvel.domain.di.register
 
 import com.ssafy.tranvel.domain.repository.RegisterRepository
 import com.ssafy.tranvel.domain.usecase.register.GetUserUseCase
+import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.scopes.ViewModelScoped
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-@Singleton
+@Module
 @InstallIn(SingletonComponent::class)
 class GetUserUseCaseModule {
 
