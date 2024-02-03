d9c9215affa9ebe631cde5832c489a17ff62cca1
DESKTOP-QHBM668\USER-PC <jeonghw4n@naver.com>
2024-02-03 19:07:28 +0900

    Fix: "ResponseDto data type 변경"
    Object -> T 변경
    
    Task: S10P12D203
    Change-Id: I84e1834dcf3f607a17da6fa7cc353b728a76854d

--- Backend/tranvel/src/main/java/com/ssafy/tranvel/dto/ResponseNullDto.java
+++ Backend/tranvel/src/main/java/com/ssafy/tranvel/dto/ResponseNullDto.java
@@ -1,22 +1,17 @@
 package com.ssafy.tranvel.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.annotation.Nullable;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 @Builder
 @AllArgsConstructor
-public class ResponseDto<T> {
+public class ResponseNullDto {
 
     private boolean result;
 
     private String msg;
-
-    private T data;
-
 
 }
