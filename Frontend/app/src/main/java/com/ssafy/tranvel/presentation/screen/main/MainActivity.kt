9b6f969635d3c08b38d961f00ff453d1e3a9c175
안성준 <hiahn777@gmail.com>
2024-02-15 12:29:54 +0900

    회원가입 화면 수정
    Change-Id: Iad7bfba2d3ddcfdf3294d7a98352f2fa063fc2e6

--- Frontend/app/src/main/java/com/ssafy/tranvel/presentation/screen/main/MainActivity.kt
+++ Frontend/app/src/main/java/com/ssafy/tranvel/presentation/screen/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.ssafy.tranvel.presentation.screen.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -9,6 +10,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.ssafy.tranvel.presentation.navigation.NavGraph
+import com.ssafy.tranvel.presentation.screen.home.navigation.homeRoute
+import com.ssafy.tranvel.presentation.screen.login.User
 import com.ssafy.tranvel.presentation.ui.theme.TRANvelTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,6 +19,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val start = if (User.id == -1L) "login_screen" else homeRoute
+        Log.d("TAG", "onCreate: $start")
         setContent {
             TRANvelTheme {
                 // A surface container using the 'background' color from the theme
@@ -24,7 +29,7 @@ class MainActivity : ComponentActivity() {
                         .background(color = Color.White)
                         .fillMaxSize(),
                 ) {
-                    NavGraph()
+                    NavGraph(start)
                 }
             }
         }
--- Frontend/app/src/main/java/com/ssafy/tranvel/presentation/screen/travel/component/GameBody.kt
+++ Frontend/app/src/main/java/com/ssafy/tranvel/presentation/screen/travel/component/GameBody.kt
@@ -3,16 +3,31 @@ package com.ssafy.tranvel.presentation.screen.travel.component
 import android.content.Context
 import android.util.Log
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.bumptech.glide.integration.compose.placeholder
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraPosition
@@ -23,7 +38,11 @@ import com.naver.maps.map.compose.Marker
 import com.naver.maps.map.compose.MarkerState
 import com.naver.maps.map.compose.NaverMap
 import com.ssafy.tranvel.presentation.screen.history.component.HistoryBody
+import com.naver.maps.map.compose.rememberCameraPositionState
+import com.ssafy.tranvel.BuildConfig
+import com.ssafy.tranvel.R
 import com.ssafy.tranvel.presentation.screen.travel.GameViewModel
+import com.ssafy.tranvel.presentation.screen.travel.LoadingIndicator
 import com.ssafy.tranvel.presentation.screen.travel.RoomInfo
 
 
@@ -39,13 +58,28 @@ fun GameBody(
     val attractionState by gameViewModel.attractionState.collectAsState()
     val enterPerson by gameViewModel.enterPerson.collectAsState()
     val enterPersonName by gameViewModel.enterPersonName.collectAsState()
+    val dialogState by gameViewModel.drawDialogState.collectAsState()
 
-    if (enterPerson){
-        Toast.makeText(context, enterPersonName,Toast.LENGTH_SHORT).show()
+    if (dialogState) {
+        LoadingIndicator()
+    }
+
+    if (enterPerson) {
+        Toast.makeText(context, enterPersonName, Toast.LENGTH_SHORT).show()
         gameViewModel.setEnterPerson()
     }
 
-    val cameraPositionState  = CameraPositionState(
+    if (gameViewModel.drawResultDialogState.value) {
+        DrawAttractionResultDialog(
+            attractionInfo.name,
+            attractionInfo.description,
+            attractionInfo.image
+        ) {
+            gameViewModel.drawResultDialogState.value = false
+        }
+    }
+
+    val cameraPositionState = CameraPositionState(
         // 카메라 초기 위치를 설정합니다.
         position = CameraPosition(
             LatLng(
@@ -96,5 +130,48 @@ fun GameBody(
             detailHistoryRecordViewModel = hiltViewModel()
         )
 
+    }
+}
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+fun DrawAttractionResultDialog(
+    attractionName: String,
+    attractionContent: String,
+    attractionImage: String?,
+    onStateChange: () -> (Unit)
+) {
+    Dialog(
+        onDismissRequest = { onStateChange() }
+    ) {
+        Column(
+            modifier = Modifier
+                .background(color = Color.White)
+                .padding(5.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = attractionName,
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 5.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+            GlideImage(
+                model = "${BuildConfig.S3_BASE_URL}${attractionImage}",
+                contentDescription = "여행지 사진",
+                failure = placeholder(R.drawable.emptyimage),
+                modifier = Modifier.size(200.dp)
+            )
+            Text(
+                text = attractionContent,
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 5.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+        }
     }
 }
